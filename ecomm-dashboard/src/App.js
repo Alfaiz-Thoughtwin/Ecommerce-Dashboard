ef67f3ec921f7307f6ad054350e6c5a583e3f38f
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-06 13:31:17 +0530

    Tut02 - React js project in Hindi # Project Setup

---
+++ ecomm-dashboard/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <>    
+      <div className="App">
+        <h1>Eommerce Project</h1><br />
+        <button>Normal Button</button>
+        <Button>React-Bootstrap Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
